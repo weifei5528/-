ddb2b1b0e3bdd8ddb5637a019b4e91b1dea77c27
weifei <weifei528@163.com>
2018-12-05 23:21:00 +0800

    添加全局的组件

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import store from './store'
 import pageHead from './components/page-head.vue'
+import pageFoot from './components/page-foot.vue'
 Vue.prototype.$store = store
 Vue.config.productionTip = false
-
+/***********注册全局的组件***************/
+Vue.component('page-head',pageHead)
+Vue.component('page-foot',pageFoot)
 App.mpType = 'app'
 
 const app = new Vue({
